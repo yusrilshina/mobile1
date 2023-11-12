4bf85fc61e8dabafbc060386bab9319d5a4945e8
yusrilshina <yusrilshina04@gmail.com>
2023-11-12 15:41:34 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,56 @@
+// Dom7
+var $ = Dom7;
+
+// Demo
+const html = document.documentElement;
+if (document.location.href.includes('safe-areas')) {
+  if (html) {
+    html.style.setProperty('--f7-safe-area-top', '44px');
+    html.style.setProperty('--f7-safe-area-bottom', '34px');
+  }
+}
+if (document.location.href.includes('example-preview')) {
+  $('.view-main').attr('data-browser-history', 'true');
+  $('.view-main').attr('data-browser-history-root', '/kitchen-sink/core/');
+  $('.view-main').attr('data-preload-previous-page', 'false');
+  $('.view-main').attr('data-ios-swipe-back', 'false');
+  document.documentElement.classList.add('example-preview');
+}
+
+html.style.setProperty('--f7-tabbar-link-active-color', '#009688');
+html.style.setProperty('--f7-tabbar-link-inactive-color', 'white');
+
+// Theme
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+if (document.location.search.indexOf('mode=') >= 0) {
+  const mode = document.location.search.split('mode=')[1].split('&')[0];
+  if (mode === 'dark') document.documentElement.classList.add('dark');
+}
+
+// Init App
+var app = new Framework7({
+  el: '#app',
+  theme,
+  // store.js,
+  store: store,
+  // routes.js,
+  routes: routes,
+  popup: {
+    closeOnEscape: true,
+  },
+  sheet: {
+    closeOnEscape: true,
+  },
+  popover: {
+    closeOnEscape: true,
+  },
+  actions: {
+    closeOnEscape: true,
+  },
+  vi: {
+    placementId: 'pltd4o7ibb9rc653x14',
+  },
+});
---
+++ js/routes.js
@@ -0,0 +1,4 @@
+var routes = [
+  { path: '/', componentUrl: 'pages/home.html' },
+  { path: '(.*)', url: 'pages/404.html' }
+];
